90e5814152d596cca7d72b95b5c9773ca06f5283
Lazhari <lazhari.mohammed@outlook.com>
2017-11-25 17:29:41 +0000

    Update the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,11 @@ const path = require('path');
 const dist = path.join(__dirname, 'dist');
 const context = path.join(__dirname, 'src');
 
-console.log(dist);
+//console.log(dist);
+module.exports = {
+  entry: `${context}/app.js`,
+  output: {
+    path: dist,
+    filename: 'blog.bundle.js'
+  }
+};
